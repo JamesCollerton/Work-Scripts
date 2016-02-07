f4bdf7fcced9a101dd6beac0081e98e10b29b8b9
James Collerton <jamescollerton@live.co.uk>
2016-02-07 14:01:26 +0000

    Installed FLASK, pushing to workingOnPhoneCall branch

--- Python_Scripts/callPhone.py
+++ Python_Scripts/callPhone.py
@@ -1,1 +1,19 @@
-# Placeholder
+import sys
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello():
+    return "Hello World!"
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# def main():
+
+# 	print("Hello")
+
+
+
+# if __name__ == "__main__":
+#     main()
