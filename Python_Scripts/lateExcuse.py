d76cd7f6424736373ef510bef024781fff65ddba
James Collerton <jamescollerton@live.co.uk>
2016-02-14 21:06:45 +0000

    Added all of the separate tube info

--- Python_Scripts/lateExcuse.py
+++ Python_Scripts/lateExcuse.py
@@ -1,10 +1,31 @@
 import requests
 
+def getTubeStatusJson():
 
+	try:
+		tubeRequest = requests.get('https://api.tfl.gov.uk/line/mode/tube/status')
+	except requests.exceptions.RequestException as e:
+		print e
+		sys.exit(1)
+
+	return(tubeRequest.json())
+
+def getTubeInfo(tubeStatusJson):
+
+	delayedTubes = []
+
+	for tubeStatus in tubeStatusJson:
+		delayedTubeInfo = {}
+		delayedTubeInfo['name'] = tubeStatus['name']
+		delayedTubeInfo['lineStatus'] = tubeStatus['lineStatuses'][0]['statusSeverityDescription']
+		delayedTubes.append(delayedTubeInfo)
+		
+	print(delayedTubes)
 
 def main():
-	r = requests.get('https://api.tfl.gov.uk/line/mode/tube/status')
-	print(r.json()[1])
+
+	tubeStatusJson = getTubeStatusJson()
+	getTubeInfo(tubeStatusJson)
 
 if __name__ == "__main__":
     main()
