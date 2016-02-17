544f0f44ab1512c125f0c85617b8004d8a5751f9
James Collerton <jamescollerton@live.co.uk>
2016-02-17 20:08:10 +0000

    Added in the string part

--- Python_Scripts/lateExcuse.py
+++ Python_Scripts/lateExcuse.py
@@ -1,12 +1,15 @@
 import requests
+import sys
 
 EMAIL_CONTENTS = "Hi <NAME>,\n\nI'm really sorry, but the <TUBE> line has a <STATUS>" + \
 				 "I will be running a few minutes late, but hopefully not too long!" + \
 				 "\n\nBest,\n\nJames"
 
-NO_DELAYED_TUBES = "\n\nNo tubes are delayed! You need a different excuse!"
+NO_DELAYED_TUBES = "\n\nNo tubes are delayed! You need a different excuse!\n\n"
 
 ERROR_CODE = 1
+
+def getCommandLineArg():
 
 def getTubeStatusJson():
 
@@ -32,22 +35,30 @@ def getTubeInfo(tubeStatusJson):
 
 def getDelayedTube(tubeStatusArray):
 
-	delayedTubeStatus = ""
-	delayedTubeName = ""
-
 	for tubeStatus in tubeStatusArray:
 		if tubeStatus['lineStatus'] != 'Good Service':
 			return(tubeStatus)			
 
+	return({'name': 'Victoria', 'lineStatus': 'Closed'})
+
 	print(NO_DELAYED_TUBES)
 	sys.exit(ERROR_CODE)
 
+def createEmailString(tubeStatus):
+
+	emailString = EMAIL_CONTENTS.replace('<TUBE>', tubeStatus['name'])
+	emailString = emailString.replace('<STATUS>', tubeStatus['lineStatus'].lower())
+
+	return(emailString)
+
 def main():
 
+	recipientName = getCommandLineArg()
 	tubeStatusJson = getTubeStatusJson()
 	tubeStatusArray = getTubeInfo(tubeStatusJson)
 	tubeStatus = getDelayedTube(tubeStatusArray)
-	print(tubeStatus)
+	emailString = createEmailString(tubeStatus)
+	print(emailString)
 
 if __name__ == "__main__":
     main()
