5e4ec05fd931d6dd355b8887af9c9b3adebcdc87
James Collerton <jamescollerton@live.co.uk>
2016-02-18 22:32:53 +0000

    Added comments to lateExcuse

--- Python_Scripts/lateExcuse.py
+++ Python_Scripts/lateExcuse.py
@@ -1,18 +1,31 @@
 import requests
 import sys
 
+# Imports the sendEmail function from the mailGunClient.py file
 from mailGunClient import *
 
+# ------------------------------------------------------------------------------ 
+# Global Variables
+
+# Email string for the body of the email. Name, tube and status will all be dynamically
+# replaced.
 EMAIL_CONTENTS = "Hi <NAME>,\n\nI'm really sorry, but the <TUBE> line has a <STATUS>. " + \
 				 "I will be running a few minutes late, but hopefully not too long!" + \
 				 "\n\nBest,\n\nJames"
 
+# Printed if there aren't any tubes delayed.
 NO_DELAYED_TUBES = "\n\nNo tubes are delayed! You need a different excuse!\n\n"
 
+# Printed if no recipient has been specified.
 ERROR_NO_COMMAND_LINE_ARGUMENTS = "\n\nInsufficient command line arguments.\n\n"
 
+# Error code for printing on system exit.
 ERROR_CODE = 1
 
+# ------------------------------------------------------------------------------ 
+# Functions
+
+# Gets the command line arguments (i.e. recipient name)
 def getCommandLineArg(commandLineArgs):
 
 	try:
@@ -23,6 +36,7 @@ def getCommandLineArg(commandLineArgs):
 
 	return(recipientName)
 
+# This gets the tube statuses using a request to the TFL API.
 def getTubeStatusJson():
 
 	try:
@@ -33,6 +47,7 @@ def getTubeStatusJson():
 
 	return(tubeRequest.json())
 
+# This strips down the tube results from the API into just the tube name and status.
 def getTubeInfo(tubeStatusJson):
 
 	tubeStatusArray = []
@@ -45,6 +60,7 @@ def getTubeInfo(tubeStatusJson):
 
 	return(tubeStatusArray)
 
+# This returns the first tube that doesn't have good service.
 def getDelayedTube(tubeStatusArray):
 
 	for tubeStatus in tubeStatusArray:
@@ -56,6 +72,8 @@ def getDelayedTube(tubeStatusArray):
 	print(NO_DELAYED_TUBES)
 	sys.exit(ERROR_CODE)
 
+# This replaces the parts of the email string (tube, status and name) with the 
+# values from the command line and the API.
 def createEmailString(tubeStatus, recipientName):
 
 	emailString = EMAIL_CONTENTS.replace('<TUBE>', tubeStatus['name'])
@@ -64,6 +82,11 @@ def createEmailString(tubeStatus, recipientName):
 
 	return(emailString)
 
+# ------------------------------------------------------------------------------ 
+# MAIN
+
+# Gets the command line arguments, tube statuses, strips them, finds the first
+# delayed tube and then dynamically creates an email string and sends it.
 def main():
 
 	recipientName = getCommandLineArg(sys.argv)
