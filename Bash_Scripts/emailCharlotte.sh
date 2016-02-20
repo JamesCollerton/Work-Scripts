dd974ddf4b812b5709b0a23ac074fe85b7aeafd2
James Collerton <jamescollerton@live.co.uk>
2016-02-20 15:35:46 +0000

    Charlotte email finished and commented

--- Bash_Scripts/emailCharlotte.sh
+++ Bash_Scripts/emailCharlotte.sh
@@ -1,4 +1,11 @@
-#This Bash script will be used to invoke the Python script to email Charlotte
+# ------------------------------------------------------------------------------
+
+# emailCharlotte.sh
+
+# This script is used so I can ping off a quick email to Charlotte from the
+# command line. It takes an argument of the email body.
+
+# ------------------------------------------------------------------------------
 
 python ../Python_Scripts/sendEmail.py SendMail MeGmail "$@"
 
